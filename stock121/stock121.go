d765bbfbc89cee83f4d81151fb4a38a03a93ede9
devFarhadCodemen <farhad@codemen.net>
2022-10-21 13:56:08 +0600

    LeetCode: Problem 121
    Solution: Best-time-to-buy-n-sell-stock

--- stock121/stock121.go
+++ stock121/stock121.go
@@ -1,42 +1,42 @@
 package stock121
 
-import "fmt"
+// import "fmt"
 
 /*####  Algorithm
 
 Input [7,1,5,3,6,4] => Output 5
-1. Initiate two pick point
-	a.  MinStockPrice =prices[0]
-	b.  MaxStockPrice =0
+1. Initiate Profit and MinStockPrice
+	a.  Profit =0
+	b.  MaxStockPrice = prices[0]
 2. Iterate through the array from 1 as i
-	if Price[i] < MinStockPrice
-		MinStockPrice=Price[i]
-	if Price[i]>MinStockPrice &&  Price[i]>MaxStockPrice
-		MaxStockPrice=Price[i]
-3. return MaxStockPrice-MinStockPrice if MaxStockPrice>MinStockPrice else 0
+	CurrentProfit = prices[i]- MinStockPrice
 
-{2,1,2,0,1}
+	if CurrentProfit > Profit ##Get the large profit always
+		Profit=CurrentProfit
+
+	if Price[i]<MinStockPrice ##Get the small pick point always
+		MinStockPrice=Price[i]
+
+3. return Profit
+
+				Input [2,4,1,7]  Output 6
+				Input [2,7,1,4]	 Output 5
 */
 func Profit(prices []int) int {
-	MinStockPrice := prices[0]
-	MaxStockPrice := 0
-	for i := 1; i < len(prices); i++ {
-		if prices[i] == 0 {
-			MinStockPrice = 0
-			MaxStockPrice = 0
 
-		} else if prices[i] < MinStockPrice && prices[i] > MaxStockPrice {
-			fmt.Println(MinStockPrice, MaxStockPrice, prices[i])
+	Profit := 0
+	MinStockPrice := prices[0]
+
+	for i := 1; i < len(prices); i++ {
+
+		CurrentProfit := prices[i] - MinStockPrice
+
+		if CurrentProfit > Profit {
+			Profit = CurrentProfit
+		}
+		if prices[i] < MinStockPrice {
 			MinStockPrice = prices[i]
 		}
-		if prices[i] > MinStockPrice && prices[i] > MaxStockPrice {
-
-			MaxStockPrice = prices[i]
-		}
 	}
-	if MaxStockPrice > MinStockPrice {
-		fmt.Println(MinStockPrice, MaxStockPrice)
-		return MaxStockPrice - MinStockPrice
-	}
-	return 0
+	return Profit
 }
--- stock121/stock121_test.go
+++ stock121/stock121_test.go
@@ -14,9 +14,15 @@ type TestCase struct {
 func TestStock121(t *testing.T){
 	testcases := []TestCase{
 		{prices: []int{7, 1, 5, 3, 6, 4},output: 5},
+		{prices: []int{7,6,4,3,1},output: 0},
 		{prices: []int{2,4,1},output: 2},
 		{prices: []int{2,4,2,1},output: 2},
 		{prices: []int{2,1,2,0,1},output: 1},
+		{prices: []int{3,2,6,5,0,3},output: 4},
+		{prices: []int{1,2},output: 1},
+		{prices: []int{2,1,2,1,0,1,2},output: 2},
+		{prices: []int{2,4,1,7},output: 6},
+		{prices: []int{2,7,1,4},output: 5},
 	}
 
 	for i,testCase := range testcases {
